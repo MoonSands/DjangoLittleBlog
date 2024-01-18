f5e74a85775bae9d63416803e456b28c812ca9ee
MoonSands <kfmty135shk@gmail.com>
2024-01-18 19:56:01 +0300

    created superuser, register model feed in admin

--- blog/article_feed/models.py
+++ blog/article_feed/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from django.urls import reverse
 class feed(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     time_create = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.title
+    def get_absolute_url(self):
+        return reverse('article',kwargs={'article_id':self.pk})
--- blog/article_feed/views.py
+++ blog/article_feed/views.py
@@ -4,10 +4,12 @@ from .models import *
 
 def index(request):
     posts=feed.objects.all()
-    return render(request, 'article_feed/index.html',{'posts':posts})
+    context = {'posts':posts}
+    return render(request, 'article_feed/index.html',context)
 
 def article(request,article_id):
     art=feed.objects.get(pk=article_id)
-    return render(request, 'article_feed/article_single.html',{'title':art.title,
-                                                  'date':art.time_create,
-                                                  'content':art.content})
+    context = {'title':art.title,
+               'date':art.time_create,
+               'content':art.content}
+    return render(request, 'article_feed/article_single.html',context)
